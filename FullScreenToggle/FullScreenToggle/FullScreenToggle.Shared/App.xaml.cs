ff88be22d1feb1834947548652660bc712e24ced
Thomas Nigro <tn@thomasnigro.fr>
2015-06-03 18:04:27 +0200

    Add Sample

---
+++ FullScreenToggle/FullScreenToggle/FullScreenToggle.Shared/App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace FullScreenToggle
+{
+    public sealed partial class App : Application
+    {
+        public App()
+        {
+            this.InitializeComponent();
+            this.Suspending += this.OnSuspending;
+        }
+        
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+
+            Frame rootFrame = Window.Current.Content as Frame;
+            
+            if (rootFrame == null)
+            {
+                rootFrame = new Frame();
+                
+                rootFrame.CacheSize = 1;
+                Window.Current.Content = rootFrame;
+            }
+
+            if (rootFrame.Content == null)
+            {
+                if (!rootFrame.Navigate(typeof(MainPage), e.Arguments))
+                {
+                    throw new Exception("Failed to create initial page");
+                }
+            }
+            Window.Current.Activate();
+        }
+
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            deferral.Complete();
+        }
+    }
+}
---
+++ FullScreenToggle/FullScreenToggle/FullScreenToggle.Shared/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Reflection;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace FullScreenToggle
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Grid_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            var view = Windows.UI.ViewManagement.ApplicationView.GetForCurrentView();
+            var runtimeMethods = view.GetType().GetRuntimeMethods();
+            
+            if (view.IsFullScreen)
+            {
+                var exitFullScreenMode = runtimeMethods.FirstOrDefault(x => x.Name == "ExitFullScreenMode");
+                exitFullScreenMode?.Invoke(view, null);
+            }
+            else
+            {
+                var tryEnterFullScreenMode = runtimeMethods.FirstOrDefault(x => x.Name == "TryEnterFullScreenMode");
+                tryEnterFullScreenMode?.Invoke(view, null);
+            }
+        }
+    }
+}
